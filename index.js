06f206993637d543ca8cf81adac269833b43439d
Susan Whiteford <swhiteford1218@gmail.com>
2023-10-24 13:59:25 -0400

    Today's Group Assignment

---
+++ index.js
@@ -0,0 +1,93 @@
+// // How do we assign a value to a variable?
+// let x = score
+
+// score = 10
+
+// const score1 = 5;
+// const score2 = 6;
+
+// let total = score1 + score2;
+
+// define(['score1, 'score2'], function() {
+// });
+
+// Set "score" to the first value in the array.
+// For each value in the array, starting with the second value:
+//    a. If the current value is greater than "score", 
+//       set "score" to the current value.
+//     b. Print "score"
+
+// 75-90% thinking, 10-25% typing
+
+// Create a variable called firstVariable
+let firstVariable;  
+
+// Assign it the value of the string "Hello World"
+firstVariable = "Hello World";
+
+// Change the value of this variable to some number
+firstVariable = 5
+
+// Store the value of firstVariable in a new variable called secondVariable
+secondVariable = firstVariable
+
+// Change the value of secondVariable to any string.
+secondVariable = 'Great job, team!'
+
+// What is the value of firstVariable?
+5
+
+// Create a variable called yourNameand set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+let yourName = 'Susan'
+let greeting = "Hello, my name is " + yourName
+console.log (greeting)
+
+// Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Susan';
+
+console.log(a < b)
+
+console.log(c > d)
+
+console.log('Name'==='Name')
+
+console.log(true||false)
+
+console.log(false && false && false && false && false || true)
+
+console.log(false === false)
+
+console.log(e === 'Susan')
+
+// console.log(a __ b ___ c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+console.log(a < b && b < c)
+
+// console.log(a __ a ___ d); // note: the answer is a simple arithmetic equation, not something "weird"
+console.log(a === a || d)
+
+// console.log(48 __ '48');
+console.log(48 == '48')
+
+// D. The farm. Declare a variable animal. Set it to be either "cow" or something else
+let animal = 'horse'
+
+// Write code that will print out "mooooo" if the it is equal to cow
+if (animal === 'cow') {
+  console.log("mooooo")
+}
+
+// Change your code so that if the variable animal is anything other than a cow, it will print "Hey! You're not a cow."
+if (animal === 'cow') {
+  console.log("mooooo")
+}else {
+  console.log("Hey! You're not a cow.")
+}
+ 
+
+
+// Commit
+
